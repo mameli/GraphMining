b578d5dbfa04438dd0cb7ae5f5b168c05372ad08
Mameli <mameli93@gmail.com>
2017-10-23 22:07:04 +0200

    notebook

--- Distance.jl
+++ Distance.jl
@@ -14,22 +14,8 @@ function randKVector(v::UnitRange{Int64}, k::Int64)
 end
 
 
-g = load("internet.gml", "graph", :gml)
+g = load("karate.gml", "graph", :gml)
 numV = nv(g)
-
-# Grafo di prova
-# g = Graph(9)
-# add_edge!(g, 1, 2)
-# add_edge!(g, 1, 3)
-# add_edge!(g, 2, 3)
-# add_edge!(g, 2, 7)
-# add_edge!(g, 2, 4)
-# add_edge!(g, 3, 5)
-# add_edge!(g, 4, 5)
-# add_edge!(g, 4, 9)
-# add_edge!(g, 5, 6)
-# add_edge!(g, 5, 8)
-# add_edge!(g, 6, 8)
 
 # Distanza precisa
 distFull = 0
